d78342cecdd7e341e634fcc573fa34eed91ba164
localghost-gif <jorn.erik.jenssen@gmail.com>
2025-09-15 19:58:18 +0200

    Update pullv2.sh

--- pullv2.sh
+++ pullv2.sh
@@ -6,7 +6,7 @@ set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 echo -e "${YELLOW}Security Wordlists Downloader (Git Raw URLs)${NC}"
 echo "============================================="
